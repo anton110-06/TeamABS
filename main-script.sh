5ab53e75aafdb2012362502e4f3484e11008096f
Antoha200 <farafonov-antosha@mail.ru>
2025-05-25 15:59:09 +0300

    Added main-script

---
+++ main-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter 1 chislo"
+read num1
+
+echo "Enter 2 chislo"
+read num2
+
+echo "Result NOD:"
+./script_GCD.sh "num1" "num2"
+
+echo "Result NOK:"
+./script_LCM.sh "num1" "num2"
